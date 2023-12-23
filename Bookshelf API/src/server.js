8266d7423896789f87ff2893b2b98e97c34296a6
Rizal Fahmi <rizalfahmi3002@gmail.com>
2023-12-23 19:18:44 +0700

    change port

--- Bookshelf API/src/server.js
+++ Bookshelf API/src/server.js
@@ -3,7 +3,7 @@ const routes = require('./routes');
 
 const init = async () => {
   const server = hapi.server({
-    port: 9000,
+    port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: true,
